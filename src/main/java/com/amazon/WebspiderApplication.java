f1a010651ba9550683c29369bee675982a17347b
jusky <2456182460@qq.com>
2017-03-02 15:57:38 +0800

    Beijing-17.3.2-test

---
+++ src/main/java/com/amazon/WebspiderApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspiderApplication.class, args);
+	}
+}
---
+++ src/main/java/com/webspider/WebSpiderApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.webspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpiderApplication.class, args);
+	}
+}
